e5f7fb39ab307ac4a0fbe6358359e857bd304d7d
Noelia_Olmos <noelia@esdir.eu>
2019-07-15 18:53:16 +0200

    Curso Gestión de proyectos arduino - ejercicios

---
+++ WriteMultipleFields-thingspeak-DHT11/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "CTLR"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "cntrTcnlgc012"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 817419			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "YPZU7K8QUSFM8NLP"   // replace XYZ with your channel write API Key
